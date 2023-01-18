6307d3701c1f68bc7f20faf35986060ad4160f0b
Xin <nshen121@gmail.com>
2023-01-18 13:41:55 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dotfiles
+
+```bash
+git clone git@github.com:nshen/dotfiles.git ~/
+~/dotfiles/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+ln -s ~/dotfiles/.config/karabiner ~/.config/karabiner
+ln -s ~/dotfiles/.zx ~/.zx
+
