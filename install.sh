50cd08531ce4aa3b935571af06ca149080fd1c3a
Xin Chen <nshen121@gmail.com>
2023-01-24 16:17:20 +0800

    fix karabiner config

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
 ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/.config/karabiner ~/.config/karabiner
+ln -s ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s ~/dotfiles/.zx ~/.zx
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig 
 
