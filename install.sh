487b9b12c9fc9dc3e1da7808f644ac6c429bf04d
Xin Chen <nshen121@gmail.com>
2024-04-29 12:22:27 +0800

    use alacritty.toml

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s ~/dotfiles/.alacritty.toml ~/.alacritty.toml
 ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s ~/dotfiles/.zx ~/.zx
